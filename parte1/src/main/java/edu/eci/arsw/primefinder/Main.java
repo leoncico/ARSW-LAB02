5cb744ae2853fdbfc1be0ec6b4d665cecd981219
leoncico <leoncico895@hotmail.com>
2024-08-22 08:14:22 -0500

    Punto 2

--- parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
+++ parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
@@ -3,12 +3,11 @@ package edu.eci.arsw.primefinder;
 public class Main {
 
 	public static void main(String[] args) {
-		
-		PrimeFinderThread pft=new PrimeFinderThread(0, 30000000);
-		
-		pft.start();
-		
-		
+		PrimeFinderThread pft1=new PrimeFinderThread(0, 10000000);
+		PrimeFinderThread pft2=new PrimeFinderThread(10000000, 20000000);
+		PrimeFinderThread pft3=new PrimeFinderThread(20000000, 30000000);
+		pft1.start();
+		pft2.start();
+		pft3.start();
 	}
-	
 }
